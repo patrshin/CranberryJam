d39c1dca493ec974f210483c5f3746f6063e0efc
patrshin <patrshin@umich.edu>
2015-11-22 03:23:32 +0900

    ItemPrefab Starting spawner

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -5,6 +5,9 @@ public class GameScript : MonoBehaviour {
 
 	public GameObject p1;
 	public GameObject p2;
+	public GameObject item;
+	float time = 0.0f;
+	float itemSpawn = 5f;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +16,7 @@ public class GameScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		time = Time.deltaTime++;
 		Vector3 p1ViewportCoords = Camera.main.WorldToViewportPoint(p1.transform.position);
 		Vector3 p2ViewportCoords = Camera.main.WorldToViewportPoint(p2.transform.position);
 		if (p1ViewportCoords.y < 0) {
