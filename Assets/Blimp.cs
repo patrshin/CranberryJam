ede7492d2e5dbb7f957785ece248ca4c5d1fc019
Noah Ehmann <nehmann@umich.edu>
2015-11-20 20:32:53 -0500

    Basic blimp prototype project

---
+++ Assets/Blimp.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blimp : MonoBehaviour {
+
+	Rigidbody2D rb;
+	public float movementSpeed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xd = Input.GetAxis("X Axis");
+		float yd = Input.GetAxis("Y Axis");
+		rb.AddForce(new Vector2(xd, yd) * 50);
+	}
+}
