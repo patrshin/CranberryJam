cb3c0c6d26266076e88f7c2cf2db95f3e3ca8b72
patrshin <patrshin@umich.edu>
2015-11-21 14:38:42 +0900

    Conflict Resolve

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class Turret : MonoBehaviour {
+
+	public int playerNum;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var inputDevice = (playerNum == 1) ? InputManager.Devices[1]: InputManager.Devices[0];
+
+	}
+}
