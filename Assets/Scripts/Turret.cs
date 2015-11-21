0c53aee0753de33b89e41f5b74db193aba7b4a91
patrshin <patrshin@umich.edu>
2015-11-21 14:56:10 +0900

    Rotating Turret

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -14,6 +14,31 @@ public class Turret : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		var inputDevice = (playerNum == 1) ? InputManager.Devices[1]: InputManager.Devices[0];
-
+		Quaternion rotate = this.transform.rotation;
+		var max = Mathf.Abs (inputDevice.RightStickY);
+		if (Mathf.Abs (inputDevice.RightStickX) > Mathf.Abs (inputDevice.RightStickY )) {
+			max = Mathf.Abs (inputDevice.RightStickX);
+		}
+		
+		//rotating player
+		if (Mathf.Abs (inputDevice.RightStickX)> 0.2 || Mathf.Abs (inputDevice.RightStickY )> 0.2) {
+			
+			Vector3 ThumbPos = new Vector3(inputDevice.RightStickX, inputDevice.RightStickY, 0);
+			Vector3 playerPos = this.transform.position - transform.parent.position;
+			
+			var angle = Vector3.Angle (playerPos, ThumbPos);
+			var cross = Vector3.Cross (playerPos, ThumbPos);
+			if (cross.z < 0) 
+				angle = -angle;
+			if (Mathf.Abs (angle) > 2) {
+				if (angle >= 0) {
+					transform.RotateAround(transform.parent.position, Vector3.forward, 10 * max);
+				}
+				else if (angle < 0){
+					transform.RotateAround(transform.parent.position, Vector3.forward, -10 * max);
+				}
+			}
+			
+		}
 	}
 }
