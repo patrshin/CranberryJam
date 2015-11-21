2f5d447bbbc298da407e1eea4d0099389220bdba
patrshin <patrshin@umich.edu>
2015-11-22 03:38:04 +0900

    Added item spawner system

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -6,8 +6,10 @@ public class GameScript : MonoBehaviour {
 	public GameObject p1;
 	public GameObject p2;
 	public GameObject item;
+	public float maxDistance; 
 	float time = 0.0f;
-	float itemSpawn = 5f;
+	float itemTimer = 5f;
+	int itemCount = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +18,7 @@ public class GameScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		time = Time.deltaTime++;
+		time += Time.deltaTime;
 		Vector3 p1ViewportCoords = Camera.main.WorldToViewportPoint(p1.transform.position);
 		Vector3 p2ViewportCoords = Camera.main.WorldToViewportPoint(p2.transform.position);
 		if (p1ViewportCoords.y < 0) {
@@ -24,6 +26,23 @@ public class GameScript : MonoBehaviour {
 		}
 		if (p2ViewportCoords.y < 0) {
 			Application.LoadLevel(2);
+		}
+		if (time > itemTimer && itemCount < 4) {
+			float randomX = (Random.value>.5f?-1:1)*Random.Range (0f,7f);
+			float randomY = (Random.value>.5f?-1:1)*Random.Range (0f,2f);
+			float distance1 = Vector3.Distance(new Vector3(randomX, randomY, 0), p1.transform.position);
+			float distance2 = Vector3.Distance(new Vector3(randomX, randomY, 0), p2.transform.position);
+			while (distance1 < maxDistance || distance2 < maxDistance) {
+				randomX = (Random.value>.5f?-1:1)*Random.Range (0f,7f);
+				randomY = (Random.value>.5f?-1:1)*Random.Range (0f,2f);
+				distance1 = Vector3.Distance(new Vector3(randomX, randomY, 0), p1.transform.position);
+				distance2 = Vector3.Distance(new Vector3(randomX, randomY, 0), p2.transform.position);
+			}
+			GameObject o = (GameObject)Instantiate (item);
+			o.transform.position = new Vector3 (randomX + transform.position.x,
+			                                    randomY + transform.position.y, 0);
+			time = 0f;
+			itemCount++;
 		}
 	}
 }
