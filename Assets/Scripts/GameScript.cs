2a60435813217292be8490dddd8b2ecf66313813
Noah Ehmann <nehmann@umich.edu>
2015-11-21 00:23:43 -0500

    Basic menus and screen flow

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScript : MonoBehaviour {
+
+	public GameObject p1;
+	public GameObject p2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 p1ViewportCoords = Camera.main.WorldToViewportPoint(p1.transform.position);
+		Vector3 p2ViewportCoords = Camera.main.WorldToViewportPoint(p2.transform.position);
+		Debug.Log (p1ViewportCoords);
+		if (p1ViewportCoords.y < 0) {
+			Application.LoadLevel(2);
+		}
+		if (p2ViewportCoords.y < 0) {
+			Application.LoadLevel(2);
+		}
+	}
+}
