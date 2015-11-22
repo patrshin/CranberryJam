f9897cdc6386f7b86af8503453c5fead10c2e8e6
patrshin <patrshin@umich.edu>
2015-11-22 13:39:19 +0900

    Fixed UI positions

--- Assets/Scripts/AbilityUI.cs
+++ Assets/Scripts/AbilityUI.cs
@@ -3,20 +3,17 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class AbilityUI : MonoBehaviour {
-	SpriteRenderer spr;
-	public Sprite boost; 
+	SpriteRenderer spr; 
 	public GameObject blimp; 
 	
 	Image cooldown;
 	
 	void Start () {
-		GameObject cooldown = GameObject.Find ("CoolDownFinalSprite");
-
 		spr = GetComponent<SpriteRenderer> ();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-
+		//if (blimp.GetComponent<Blimp>.abi
 	}
 }
