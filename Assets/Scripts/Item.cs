7ef827c95718799c55fb77c499e33e9f563867d1
Noah Ehmann <nehmann@umich.edu>
2015-11-21 23:36:39 -0500

    Use powerup icons in world

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -5,20 +5,29 @@ public class Item : MonoBehaviour {
 	public ItemType item;
 	public float offset = 0f;
 
+	public Sprite cannonSprite;
+	public Sprite dumpSprite;
+	public Sprite boostSprite;
+
 	// Use this for initialization
 	void Start () {
 		float rand = Random.Range (0f, 1.0F) + offset;;
 		if (rand < 0.3) {
 			item = ItemType.CANNON;
+			GetComponent<SpriteRenderer>().sprite = cannonSprite;
 		} else {
+			GetComponent<SpriteRenderer>().sprite = dumpSprite;
 			item = ItemType.DUMP;
 		} 
 		if (item == ItemType.DUMP) {
+			GetComponent<SpriteRenderer>().sprite = dumpSprite;
 			gameObject.GetComponent<Renderer> ().material.color = Color.white;
 		}
 		if (item == ItemType.BOOST) {
+			GetComponent<SpriteRenderer>().sprite = boostSprite;
 			gameObject.GetComponent<Renderer> ().material.color = Color.green;
 		}	if (item == ItemType.CANNON) {
+			GetComponent<SpriteRenderer>().sprite = cannonSprite;
 			gameObject.GetComponent<Renderer> ().material.color = Color.red;
 		}
 	}
