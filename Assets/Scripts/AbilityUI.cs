beb23134393ef44550c955085db3e94733e8ba66
patrshin <patrshin@umich.edu>
2015-11-22 13:11:57 +0900

    Fixed Ability and Item UI a bit

---
+++ Assets/Scripts/AbilityUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AbilityUI : MonoBehaviour {
+	SpriteRenderer spr;
+	public Sprite boost; 
+	public GameObject blimp; 
+	
+	Image cooldown;
+	
+	void Start () {
+		GameObject cooldown = GameObject.Find ("CoolDownFinalSprite");
+
+		spr = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+	}
+}
